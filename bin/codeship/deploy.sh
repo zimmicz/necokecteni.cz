b6733b85e59f7b1e74597b653d46a0a65803b1ec
Michal Zimmermann <zimmicz@gmail.com>
2019-10-21 22:07:01 +0200

    Fix build

--- bin/codeship/deploy.sh
+++ bin/codeship/deploy.sh
@@ -7,7 +7,7 @@ git checkout gh-pages && \
 git checkout master -- CNAME && \
 git commit --amend --no-edit && \
 git push ${REMOTE_REPOSITORY} :${REMOTE_BRANCH} && \
-git push ${REMOTE_REPOSITORY} ${REMOTE_BRANCH} && \
+git push --force ${REMOTE_REPOSITORY} ${REMOTE_BRANCH} && \
 git checkout master && \
 git branch -D ${REMOTE_BRANCH}
 
