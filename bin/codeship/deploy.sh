001d641ddfd19e45b428d00dfe96c92165ced793
Michal Zimmermann <zimmicz@gmail.com>
2019-10-21 21:55:13 +0200

    Use variables

--- bin/codeship/deploy.sh
+++ bin/codeship/deploy.sh
@@ -2,12 +2,12 @@
 
 cd ~/clone
 
-git subtree split --prefix site -b gh-pages && \
+git subtree split --prefix site -b ${REMOTE_BRANCH} && \
 git checkout gh-pages && \
 git checkout master -- CNAME && \
 git commit --amend --no-edit && \
-git push origin :gh-pages && \
-git push origin && \
+git push ${REMOTE_REPOSITORY} :${REMOTE_BRANCH} && \
+git push ${REMOTE_REPOSITORY} ${REMOTE_BRANCH} && \
 git checkout master && \
-git branch -D gh-pages
+git branch -D ${REMOTE_BRANCH}
 
