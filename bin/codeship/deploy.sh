398a2441e16bbceb4abb2a3e3dd4bb45ab224b3c
Michal Zimmermann <zimmicz@gmail.com>
2019-10-21 22:17:39 +0200

    Don't force push

--- bin/codeship/deploy.sh
+++ bin/codeship/deploy.sh
@@ -6,8 +6,7 @@ git subtree split --prefix site -b ${REMOTE_BRANCH} && \
 git checkout gh-pages && \
 git checkout master -- CNAME && \
 git commit --amend --no-edit && \
-git push ${REMOTE_REPOSITORY} :${REMOTE_BRANCH} && \
-git push --force ${REMOTE_REPOSITORY} ${REMOTE_BRANCH} && \
+git push ${REMOTE_REPOSITORY} ${REMOTE_BRANCH} && \
 git checkout master && \
 git branch -D ${REMOTE_BRANCH}
 
