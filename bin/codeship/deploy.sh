45b76dba72255bca7ad9ed34bfc29dbdafe860c0
Michal Zimmermann <zimmicz@gmail.com>
2019-10-21 21:44:09 +0200

    Add deployment script

--- bin/codeship/deploy.sh
+++ bin/codeship/deploy.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-ls -l
+cd ~/clone
+
+git subtree split --prefix site -b gh-pages && \
+git checkout gh-pages && \
+git checkout master -- CNAME && \
+git commit --amend --no-edit && \
+git push origin :gh-pages && \
+git push origin && \
+git checkout master && \
+git branch -D gh-pages
 
