dbeab9202155fc175c3162e2eda5f4bd77a9a2ca
Michal Zimmermann <zimmicz@gmail.com>
2019-10-21 21:21:15 +0200

    Integrate codeship

--- publishconf.py
+++ publishconf.py
@@ -19,8 +19,4 @@ CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 OUTPUT_PATH = 'site/'
-
-# Following items are often useful when publishing
-
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+PRODUCTION = True
