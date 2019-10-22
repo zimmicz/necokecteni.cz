ae5daffee8c6214c2738441397081395babe3039
Michal Zimmermann <zimmicz@gmail.com>
2019-10-22 14:07:23 +0200

    Force push

--- bin/codeship/deploy.sh
+++ bin/codeship/deploy.sh
@@ -6,14 +6,15 @@ pip install -r requirements.txt
 yarn install
 make publish
 
-git fetch origin --unshallow || true
 git fetch origin "+refs/heads/*:refs/remotes/origin/*"
-git add -f site
-git commit -m "Deploy"
-git subtree split --prefix site -b ${REMOTE_BRANCH}
-git checkout -- .
-git checkout ${REMOTE_BRANCH}
-git pull --rebase ${REMOTE_REPOSITORY}/${REMOTE_BRANCH}
-git push ${REMOTE_REPOSITORY} ${REMOTE_BRANCH}
-git checkout master
-git branch -D ${REMOTE_BRANCH}
+
+git add -f site && \
+git commit -m "Deploy" && \
+git subtree split --prefix site -b ${REMOTE_BRANCH} && \
+git checkout -- . && \
+git checkout ${REMOTE_BRANCH} && \
+git branch --set-upstream-to=origin/${REMOTE_BRANCH} ${REMOTE_BRANCH}
+git pull --ff-only origin && \
+git push ${REMOTE_REPOSITORY} ${REMOTE_BRANCH} && \
+git checkout master && \
+git branch -D ${REMOTE_BRANCH} && \
