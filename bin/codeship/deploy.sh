f8e3179d63d3bfac1b22a1f4246a5e6be68b9022
Michal Zimmermann <zimmicz@gmail.com>
2019-10-22 14:09:29 +0200

    Force push

--- bin/codeship/deploy.sh
+++ bin/codeship/deploy.sh
@@ -8,13 +8,13 @@ make publish
 
 git fetch origin "+refs/heads/*:refs/remotes/origin/*"
 
-git add -f site && \
-git commit -m "Deploy" && \
-git subtree split --prefix site -b ${REMOTE_BRANCH} && \
-git checkout -- . && \
-git checkout ${REMOTE_BRANCH} && \
-git branch --set-upstream-to=origin/${REMOTE_BRANCH} ${REMOTE_BRANCH}
-git pull --ff-only origin && \
-git push ${REMOTE_REPOSITORY} ${REMOTE_BRANCH} && \
-git checkout master && \
-git branch -D ${REMOTE_BRANCH} && \
+# git add -f site && \
+# git commit -m "Deploy" && \
+# git subtree split --prefix site -b ${REMOTE_BRANCH} && \
+# git checkout -- . && \
+# git checkout ${REMOTE_BRANCH} && \
+# git branch --set-upstream-to=origin/${REMOTE_BRANCH} ${REMOTE_BRANCH}
+# git pull --ff-only origin && \
+# git push ${REMOTE_REPOSITORY} ${REMOTE_BRANCH} && \
+# git checkout master && \
+# git branch -D ${REMOTE_BRANCH} && \
