b4fca0f6472e8be95b13a4c306c7aaffcf5cb381
Michal Zimmermann <zimmicz@gmail.com>
2019-10-22 13:35:31 +0200

    Remove CNAME

--- bin/codeship/deploy.sh
+++ bin/codeship/deploy.sh
@@ -2,13 +2,15 @@
 
 cd ~/clone
 sed -i '/pkg-resources/d' requirements.txt
+sed -i '/site/d' .gitignore
 pip install -r requirements.txt
 yarn install
 make publish
 
-git add . && \
+git add site && \
 git commit -m "Deploy" && \
 git subtree split --prefix site -b ${REMOTE_BRANCH} && \
+git checkout -- . && \
 git checkout ${REMOTE_BRANCH} && \
 git push --force ${REMOTE_REPOSITORY} ${REMOTE_BRANCH} && \
 git checkout master && \
