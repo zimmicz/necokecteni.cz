e964328c7f554dc771bcf00662785ab1e3913367
Michal Zimmermann <zimmicz@gmail.com>
2019-10-22 12:58:49 +0200

    Fix deployment

--- bin/codeship/deploy.sh
+++ bin/codeship/deploy.sh
@@ -4,7 +4,6 @@ cd ~/clone
 
 git subtree split --prefix site -b ${REMOTE_BRANCH} && \
 git checkout gh-pages && \
-git push force ${REMOTE_REPOSITORY} ${REMOTE_BRANCH} && \
+git push ${REMOTE_REPOSITORY} ${REMOTE_BRANCH} && \
 git checkout master && \
 git branch -D ${REMOTE_BRANCH}
-
