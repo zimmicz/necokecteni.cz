f53cde07b9dc33d6c2de38258f406ec8be7f913a
Michal Zimmermann <zimmicz@gmail.com>
2019-10-22 14:41:33 +0200

    Final version

--- bin/codeship/deploy.sh
+++ bin/codeship/deploy.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-cd ~/clone
-sed -i '/pkg-resources/d' requirements.txt
-pip install -r requirements.txt
-yarn install
-make publish
+cd ~/clone && \
+sed -i '/pkg-resources/d' requirements.txt && \
+pip install -r requirements.txt && \
+yarn install && \
+make publish && \
 
-git fetch origin "+refs/heads/*:refs/remotes/origin/*"
-git remote set-branches --add origin ${REMOTE_BRANCH}
-git add -f site
-git commit -m "Deploy"
-git checkout -- .
-git checkout ${REMOTE_BRANCH}
-git checkout master -- site
-rsync -av --progress site/ ./
-git reset HEAD site
-rm -rf site __pycache__ node_modules plugins
-
+git fetch origin "+refs/heads/*:refs/remotes/origin/*" && \
+git remote set-branches --add origin ${REMOTE_BRANCH} && \
+git checkout master && \
+git add -f site && \
+git commit -m "Deploy" && \
+git checkout -- . && \
+git checkout ${REMOTE_BRANCH} && \
+git checkout master -- site && \
+rsync -av --progress site/ ./ && \
+git reset HEAD site && \
+rm -rf site __pycache__ node_modules plugins && \
+git add . && \
+git commit -m "Deploy" && \
+git push
