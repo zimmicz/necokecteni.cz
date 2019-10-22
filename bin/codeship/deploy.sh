bd126581a2533cf56bf8a9278d777bb86422dace
Michal Zimmermann <zimmicz@gmail.com>
2019-10-22 14:33:03 +0200

    Test2

--- bin/codeship/deploy.sh
+++ bin/codeship/deploy.sh
@@ -9,8 +9,8 @@ make publish
 git fetch origin "+refs/heads/*:refs/remotes/origin/*"
 git remote set-branches --add origin ${REMOTE_BRANCH}
 git add -f site
-git checkout -- .
 git commit -m "Deploy"
+git checkout -- .
 git checkout ${REMOTE_BRANCH}
 git checkout master -- site
 rsync -av --progress site/ ./
