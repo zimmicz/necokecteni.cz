15b77a4a63c2eab263797f0e987a25445c795472
Michal Zimmermann <zimmicz@gmail.com>
2019-10-22 13:55:11 +0200

    Don't force push

--- bin/codeship/deploy.sh
+++ bin/codeship/deploy.sh
@@ -2,16 +2,18 @@
 
 cd ~/clone
 sed -i '/pkg-resources/d' requirements.txt
-sed -i '/site/d' .gitignore
 pip install -r requirements.txt
 yarn install
 make publish
 
-git add site && \
-git commit -m "Deploy" && \
-git subtree split --prefix site -b ${REMOTE_BRANCH} && \
-git checkout -- . && \
-git checkout ${REMOTE_BRANCH} && \
-git push ${REMOTE_REPOSITORY} ${REMOTE_BRANCH} && \
-git checkout master && \
+git fetch origin --unshallow || true
+git fetch origin "+refs/heads/*:refs/remotes/origin/*"
+git add -f site
+git commit -m "Deploy"
+git subtree split --prefix site -b ${REMOTE_BRANCH}
+git checkout -- .
+git checkout ${REMOTE_BRANCH}
+git pull --rebase ${REMOTE_REPOSITORY}/${REMOTE_BRANCH}
+git push ${REMOTE_REPOSITORY} ${REMOTE_BRANCH}
+git checkout master
 git branch -D ${REMOTE_BRANCH}
