32a77ce8efdaee3beaa4759e3a7c8983ae97af9e
Michal Zimmermann <zimmicz@gmail.com>
2019-10-22 12:56:30 +0200

    Add CNAME to extra folder

--- bin/codeship/deploy.sh
+++ bin/codeship/deploy.sh
@@ -4,9 +4,7 @@ cd ~/clone
 
 git subtree split --prefix site -b ${REMOTE_BRANCH} && \
 git checkout gh-pages && \
-git checkout master -- CNAME && \
-git commit --amend --no-edit && \
-git push ${REMOTE_REPOSITORY} ${REMOTE_BRANCH} && \
+git push force ${REMOTE_REPOSITORY} ${REMOTE_BRANCH} && \
 git checkout master && \
 git branch -D ${REMOTE_BRANCH}
 
