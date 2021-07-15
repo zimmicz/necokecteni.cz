e2c3e4a7a44275838e95c85263e5abd814fee6bc
Michal Zimmermann <zimmicz@gmail.com>
2021-07-15 08:02:40 +0200

    Drop sidebar

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from './index.module.css';
 import { Content } from '../Content';
 import { Header } from '../Header';
-import { Sidebar } from '../Sidebar';
 
 type Props = {
     children: {
@@ -16,7 +15,6 @@ export const Layout = ({ children }: Props) => {
         <div className={styles.layout}>
             <Header className={styles.header} />
             <Content className={styles.content}>{children.content}</Content>
-            <Sidebar className={styles.sidebar}>{children.sidebar}</Sidebar>
         </div>
     );
 };
