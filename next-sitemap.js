3ecbf3d5c36c296eda4eebae6ef3f816947fc203
Michal Zimmermann <zimmicz@gmail.com>
2021-07-15 14:55:29 +0200

    Add sitemap.xml and robots.txt

---
+++ next-sitemap.js
@@ -0,0 +1,25 @@
+module.exports = {
+  siteUrl: 'https://www.necokecteni.cz',
+  changefreq: 'monthly',
+  priority: 0.7,
+  sitemapSize: 5000,
+  generateRobotsTxt: true,
+  // Default transformation function
+  transform: async (config, path) => {
+    return {
+      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    }
+  },
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
+};
