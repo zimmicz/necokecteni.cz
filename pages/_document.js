7d8b05ac139e92e2689c7d49e3ea351bae9f350a
Michal Zimmermann <zimmicz@gmail.com>
2021-08-02 12:23:22 +0200

    feat: add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,17 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@400;700&family=Barlow:wght@400;500;700&display=swap" rel="stylesheet" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-150612201-1"></script>
+          <script async
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'UA-150612201-1');`
+            }}
+          >
+          </script>
         </Head>
         <body>
           <Main />
